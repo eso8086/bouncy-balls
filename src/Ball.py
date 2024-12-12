6d88bc7fc018aac07884d19512b88a0d0a65fe27
ElsevarAsadov <elseverclash57@gmail.com>
2024-12-12 11:37:41 -0800

    removed unnecessary prop

--- src/Ball.py
+++ src/Ball.py
@@ -12,7 +12,6 @@ class Ball:
             self.velocity = vec2(0, 0)
             self.speed = self.velocity.length()
             self.acceleration = vec2(0, 0)
-            self.can_move = False
 
         def update(self, dt: int):
             self._handle_controls()
@@ -26,14 +25,13 @@ class Ball:
                 self.center += self.velocity.normalize() * self.speed * dt
 
 
-            if self.speed < 5:
+            if abs(self.speed) < 5:
                 self.velocity = vec2(0, 0)
 
             self.velocity *= 0.3 ** dt
             self.acceleration = vec2(0, 0)
 
 
-            collided = True
 
             # collision:
             if self._check_collision_with_right():
@@ -52,31 +50,29 @@ class Ball:
                 self.center.y = 500 - self.radius
                 self.velocity.y = self.velocity.y * -1
 
-            else: collided = False
-
-            self.can_move = not collided
-
         def draw(self, surface: pg.surface.Surface):
             pg.draw.circle(surface, "red", self.center, self.radius)
 
 
         def _handle_controls(self):
-             if self.can_move:
-                keys = pg.key.get_pressed()
-                if keys[pg.K_w]:
-                    self.acceleration.y = -80
+            keys = pg.key.get_pressed()
+            if keys[pg.K_w]:
+                self.acceleration.y = -80
 
-                if keys[pg.K_a]:
-                    self.acceleration.x = -80
+            if keys[pg.K_a]:
+                self.acceleration.x = -80
 
-                if keys[pg.K_d]:
-                    self.acceleration.x = 80
+            if keys[pg.K_d]:
+                self.acceleration.x = 80
 
-                if keys[pg.K_s]:
-                    self.acceleration.y = 80
-                
-                if keys[pg.K_SPACE]:
-                    self.acceleration = vec2(randrange(-1, 1)* 300, randrange(-1, 1) * 300)
+            if keys[pg.K_s]:
+                self.acceleration.y = 80
+            
+
+            for event in pg.event.get(eventtype=pg.KEYDOWN):
+                if event.key == pg.K_SPACE:
+                    self.acceleration = vec2(randrange(-1200, 1200), randrange(-1200, 1200))
+
 
         def _check_collision_with_right(self):
             return self.center.x >= 500 - self.radius
