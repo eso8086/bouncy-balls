706dcc9c9eaf0a7d524122dba65fdb0ca03288ee
eso8086 <elicodingyt@gmail.com>
2024-12-19 13:53:30 +0400

    refactored

--- src/ball.py
+++ src/ball.py
@@ -1,6 +1,7 @@
 import pygame as pg
 from random import randrange
 from pygame import Vector2 as vec2
+from pygame.key import ScancodeWrapper
 
 GRAVITY = vec2(0, -20)
 class Ball:
@@ -13,24 +14,37 @@ class Ball:
             self.speed = self.velocity.length()
             self.acceleration = vec2(0, 0)
 
-        def update(self, dt: int):
-            self._handle_controls()
+        def update(self, dt: int, events: list[pg.event.Event], keys: ScancodeWrapper):
+
+            if keys[pg.K_w]:
+                self.acceleration.y = -80
+
+            if keys[pg.K_a]:
+                self.acceleration.x = -80
+
+            if keys[pg.K_d]:
+                self.acceleration.x = 80
+
+            if keys[pg.K_s]:
+                self.acceleration.y = 80
+
+            for event in events:
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_SPACE:
+                        self.acceleration = vec2(randrange(-1200, 1200), randrange(-1200, 1200))
 
             self.acceleration -= GRAVITY
-
             self.velocity += self.acceleration
             self.speed = self.velocity.length()
 
-            if self.speed != 0:
+            if self.velocity.length() != 0: # if speed is not 0
                 self.center += self.velocity.normalize() * self.speed * dt
-
 
             if abs(self.speed) < 5:
                 self.velocity = vec2(0, 0)
 
             self.velocity *= 0.3 ** dt
             self.acceleration = vec2(0, 0)
-
 
 
             # collision:
@@ -53,25 +67,6 @@ class Ball:
         def draw(self, surface: pg.surface.Surface):
             pg.draw.circle(surface, "red", self.center, self.radius)
 
-
-        def _handle_controls(self):
-            keys = pg.key.get_pressed()
-            if keys[pg.K_w]:
-                self.acceleration.y = -80
-
-            if keys[pg.K_a]:
-                self.acceleration.x = -80
-
-            if keys[pg.K_d]:
-                self.acceleration.x = 80
-
-            if keys[pg.K_s]:
-                self.acceleration.y = 80
-            
-
-            for event in pg.event.get(eventtype=pg.KEYDOWN):
-                if event.key == pg.K_SPACE:
-                    self.acceleration = vec2(randrange(-1200, 1200), randrange(-1200, 1200))
 
 
         def _check_collision_with_right(self):
--- src/main.py
+++ src/main.py
@@ -1,48 +1,11 @@
-import pygame as pg
-from ball import Ball
-from pygame import Vector2 as vec2
+from application import Application
+
 
 if __name__ == "__main__":
-    running = False
-    dt = 0
+    app = Application()
 
-    def init():
-        global screen, clock, running
-        pg.init()
-        screen  = pg.display.set_mode((500, 500))
-        clock = pg.time.Clock()
-        running = True
+    while app.running:
+        app.update()
+        app.render()
 
-
-    def handle_keyboard_inputs():
-        global running
-        keys = pg.key.get_pressed()
-
-        if keys[pg.K_ESCAPE]:
-            running = False
-
-
-    init()
-
-    circles = [Ball(center=vec2(screen.get_rect().center))]
-
-    while running:
-        for event in pg.event.get(eventtype=pg.QUIT):
-            if event.type == pg.QUIT:
-                running = False
-
-
-        handle_keyboard_inputs()
-
-        screen.fill("yellow")
-
-
-        for circle in circles:
-            circle.update(dt)
-            circle.draw(screen)
-
-        pg.display.flip()
-        dt = clock.tick(pg.display.get_current_refresh_rate()) * .001
-        
-    pg.quit()
-
+    app.exit()
